1ce9c04448a99c8356e54ed7785cf8cf88bb90a5
Cuong Nguyen <cuongnb14@gmail.com>
2021-03-12 09:35:34 +0700

    Auto create admin account if not exists

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -e
+
+# Create admin account if not exists
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # get the currently active user model,
+
+User.objects.filter(username='admin').exists() or \
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+EOF
+
+
 python3 manage.py collectstatic --noinput
 honcho start
