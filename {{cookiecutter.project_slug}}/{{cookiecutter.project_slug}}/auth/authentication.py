eda1035ccfe36e53733e7927253c3e44598302cd
Cuong Nguyen <cuongnb14@gmail.com>
2021-04-19 14:26:43 +0700

    optimize custom jwt auth

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/auth/authentication.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/auth/authentication.py
@@ -2,26 +2,32 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
 
 
-class UserOrAnonymousJWTAuthentication(JWTAuthentication):
+class UserJWTOrSIDAuthentication(JWTAuthentication):
     """
     Auth by JWT token or session ID for anonymous user
     """
     def authenticate(self, request):
-        header = self.get_header(request)
-        if header is None:
-            raise InvalidToken()
+        try:
+            header = self.get_header(request)
+            if header is None:
+                return None
 
-        if header.decode().upper().startswith("SID"):
-            request.anonymous_id = self.get_raw_token(header).decode()
-            return None
-        else:
-            raw_token = self.get_raw_token(header)
-            if raw_token is None:
-                raise InvalidToken()
+            if header.decode().upper().startswith("SID"):
+                request.anonymous_id = self.get_raw_token(header).decode()
+                return None
+            else:
+                raw_token = self.get_raw_token(header)
+                if raw_token is None:
+                    return None
 
-            validated_token = self.get_validated_token(raw_token)
+                validated_token = self.get_validated_token(raw_token)
 
-            return self.get_user(validated_token), validated_token
+                return self.get_user(validated_token), validated_token
+
+        except InvalidToken as e:  # Convert to standard exceptions
+            raise InvalidTokenError(e.detail["detail"])
+        except AuthenticationFailed as e:
+            raise AuthenticationError(e.detail["detail"])
 
 
 class UserJWTAuthentication(JWTAuthentication):
@@ -29,14 +35,9 @@ class UserJWTAuthentication(JWTAuthentication):
     Auth by JWT token or session ID for anonymous user
     """
     def authenticate(self, request):
-        header = self.get_header(request)
-        if header is None:
-            raise InvalidToken()
-
-        raw_token = self.get_raw_token(header)
-        if raw_token is None:
-            raise InvalidToken()
-
-        validated_token = self.get_validated_token(raw_token)
-
-        return self.get_user(validated_token), validated_token
+        try:
+            super().authenticate(request)
+        except InvalidToken as e:  # Convert to standard exceptions
+            raise InvalidTokenError(e.detail["detail"])
+        except AuthenticationFailed as e:
+            raise AuthenticationError(e.detail["detail"])
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/auth/exceptions.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class InvalidTokenError(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = _('Token is invalid or expired')
+    default_code = 'invalid_access_token'
+
+
+class AuthenticationError(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = _('Incorrect authentication credentials')
+    default_code = 'authentication_failed'
