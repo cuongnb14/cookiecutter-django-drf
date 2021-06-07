357edb4d6da904b039bd82c7babdd98fa9f0d435
Cuong Nguyen <cuongnb14@gmail.com>
2021-06-07 11:58:48 +0700

    fix requirements

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -117,6 +117,8 @@ DATABASES = {
 }
 {%- endif %}
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
