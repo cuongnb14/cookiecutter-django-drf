ce95bfdc3728948a347c36ecb1b0cb6efeec8968
Cuong Nguyen <cuongnb14@gmail.com>
2021-07-07 11:07:55 +0700

    Update entrypoint.sh

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Create admin account if not exists
-cat <<EOF | python manage.py shell
-from django.contrib.auth import get_user_model
+# cat <<EOF | python manage.py shell
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()  # get the currently active user model,
+# User = get_user_model()  # get the currently active user model,
 
-User.objects.filter(username='admin').exists() or \
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-EOF
+# User.objects.filter(username='admin').exists() or \
+#     User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+# EOF
 
 
 python3 manage.py collectstatic --noinput
