663991b76dc0b71e1b2bb585bd0257dbc11b77ed
Cuong Nguyen <cuongnbzx@gmail.com>
2022-03-03 14:48:44 +0700

    upgrade django 4.x

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -1,18 +1,3 @@
-"""{{cookiecutter.project_slug}} URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -38,6 +23,6 @@ admin.site.site_header = '{{cookiecutter.project_name}} Administration'
 
 if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
-            import debug_toolbar
+        import debug_toolbar
 
-            urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
