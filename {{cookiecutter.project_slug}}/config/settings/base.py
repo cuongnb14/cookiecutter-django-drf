869b3df67e5dc57a8cfa8a8aba87e13b0c9c42e4
Cuong Nguyen <cuongnbzx@gmail.com>
2022-03-17 20:40:25 +0700

    refactor

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -276,13 +276,13 @@ REST_FRAMEWORK = {
         '{{cookiecutter.project_slug}}.auth.authentication.UserJWTAuthentication',
     ),
 
-    'EXCEPTION_HANDLER': 'base.exceptions.handler.custom_exception_handler',
+    'EXCEPTION_HANDLER': 'common.exceptions.handler.custom_exception_handler',
 
-    'DEFAULT_PAGINATION_CLASS': 'base.pagination.StandardPagination',
+    'DEFAULT_PAGINATION_CLASS': 'common.pagination.StandardPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
     'DEFAULT_THROTTLE_CLASSES': [
-        'base.throttling.CloudflareScopedRateThrottle',
+        'common.throttling.CloudflareScopedRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
         'send_email': '1/min',
