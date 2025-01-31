d8b66af3726491097abd32d41a884f668d807d2e
Cuong Nguyen <cuongnb14@gmail.com>
2025-01-31 21:28:59 +0700

    refactor user model

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/users/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    # First and last name do not cover name patterns around the globe
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = None  # type: ignore[assignment]
+    last_name = None  # type: ignore[assignment]
 
     def __str__(self):
         return self.username
