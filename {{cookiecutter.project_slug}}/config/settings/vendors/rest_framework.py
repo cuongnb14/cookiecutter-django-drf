e7e59c344d56e9707ffb5837f68eacecc516a8bd
Cuong Nguyen <cuongnbzx@gmail.com>
2025-02-05 21:17:49 +0700

    Update DEFAULT_RENDERER_CLASSES

--- {{cookiecutter.project_slug}}/config/settings/vendors/rest_framework.py
+++ {{cookiecutter.project_slug}}/config/settings/vendors/rest_framework.py
@@ -1,5 +1,8 @@
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_RENDERER_CLASSES": (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
